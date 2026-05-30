67381a506b01bb70c3727522fc4ee1e9a548d5b5
Bittu Kumar | Frontend Developer <bk3631134@gmail.com>
2026-05-30 10:49:20 +0530

    Refactor server port configuration for flexibility

--- server.js
+++ server.js
@@ -53,4 +53,8 @@ app.get('/download-file', async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-app.listen(3000,()=>console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
